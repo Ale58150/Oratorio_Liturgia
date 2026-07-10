b16235fc2672690b72f4e4f6665f386b95ee14ab
@ Alejandro_Zapata <142756173+Ale58150@users.noreply.github.com>
2026-07-10 01:20:53 -0400

    Configurar conexión MySQL con variables de Railway

--- servidor/conexionBD.php
+++ servidor/conexionBD.php
@@ -1,8 +1,23 @@
+
+//$conexion = new mysqli("localhost", "root", "1234", "oratorio");
+
+//if ($conexion->connect_error) {
+   // die("Error de conexión: " . $conexion->connect_error);
+//}
+
 <?php
-$conexion = new mysqli("localhost", "root", "1234", "oratorio");
+
+$host = getenv('MYSQLHOST');
+$puerto = getenv('MYSQLPORT');
+$base = getenv('MYSQLDATABASE');
+$usuario = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+
+$conexion = new mysqli($host, $usuario, $password, $base, (int)$puerto);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
+$conexion->set_charset("utf8");
 ?>
